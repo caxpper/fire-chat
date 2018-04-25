15a1e0ee5f419c34c4a30446542cc72652d515dd
Alejandro Gaspar <caxpper@hotmail.com>
2018-04-25 14:41:19 -0700

    Setup firebase and pulled message from db

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+    firebase:{
+        apiKey: "AIzaSyDjB-YzTvqQKSaGE20Mu6Qoh1SQLOKcYzg",
+        authDomain: "fire-chat-8ab5f.firebaseapp.com",
+        databaseURL: "https://fire-chat-8ab5f.firebaseio.com",
+        projectId: "fire-chat-8ab5f",
+        storageBucket: "fire-chat-8ab5f.appspot.com",
+        messagingSenderId: "838601008011"
+    }
+}
